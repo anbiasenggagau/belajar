eca6ee5a1707bea2381492bf8b27e35bbc28fbb2
Muhammad Anbia Senggagau <muhammadsenggagau@gmail.com>
2021-12-22 17:01:46 +0700

    Python

--- Python Project/6. Formatted Strings/main.py
+++ Python Project/6. Formatted Strings/main.py
@@ -6,3 +6,13 @@ print(message)
 #Example of formatted strings
 msg = f'{firstName} [{lastName}] is a coder'
 print(msg)
+
+# Another formated string
+print('This is a string {}'.format('INSERTED'))
+print('The {} {} {}'.format('fox','brown','quick'))
+print('The {2} {0} {1}'.format('fox','brown','quick'))
+print('The {1} {1} {1}'.format('fox','brown','quick'))
+
+print()
+
+print('The {f} {b} {q}'.format(f='fox',b='brown',q='quick'))
