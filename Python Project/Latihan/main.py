fa377a148655ac8fe772d67e7daf27162f74dcd8
Muhammad Anbia Senggagau <muhammadsenggagau@gmail.com>
2021-12-27 15:22:02 +0700

    One step closer

---
+++ Python Project/Latihan/main.py
@@ -0,0 +1,40 @@
+# st = "Print only the words that start with s in this sentence"
+# st = st.split(' ')
+# for item in st:
+# 	if (item[0] == 's'):
+# 		print(item)
+# print()
+# ============================================================================
+
+# for item in range(0,11,2):
+# 	print(item)
+# print()
+# ============================================================================
+
+# list = [ x for x in range(1,50) if x%3==0 ]
+# print(list)
+# print()
+# ============================================================================
+
+# st = "Print every word in this sentence that has an even number of letters"
+# st = st.split(' ')
+# for item in st:
+# 	if(len(item)%2 == 0):
+# 		print(f"{item} even")
+# print()
+# ============================================================================
+
+# for item in range(1,20):
+# 	if(item%3 == 0):
+# 		print(f"{item} Fizz")
+# 	if(item%5 == 0):
+# 		print(f"{item} Buzz")
+# print()
+# ============================================================================
+
+# st = 'Create a list of the first letters of every word in this string'
+# st = st.split(' ')
+# list = [item[0] for item in st]
+# print(list)
+
+help(list.insert)
