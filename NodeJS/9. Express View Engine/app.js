6b7fcef2a80c2588208454dd28032fa9a2705d46
Muhammad Anbia Senggagau <muhammadsenggagau@gmail.com>
2022-01-12 08:10:24 +0700

    Progressing Node JS

--- NodeJS/9. Express View Engine/app.js
+++ NodeJS/9. Express View Engine/app.js
@@ -1,10 +1,13 @@
 const express=require('express');
+const expressLayouts=require('express-ejs-layouts');
 const app=express();
 const port=3000;
 
 // Memuat Menggunakan EJS
 // secara otomatis, object app akan memeriksa file yang ada di folder views
 app.set('view engine', 'ejs');
+// Inisialisasi package express layouts
+app.use(expressLayouts);
 
 const mahasiswa=[
     {
@@ -23,16 +26,18 @@ const mahasiswa=[
 
 app.get('/', (req, res) => {
     // Parameter kedua merupakan pengiriman variable, ke file index
-    res.render('index', { nama: 'Anbia', mahasiswa: mahasiswa });
+    // Dengan menggunakan package express layouts, kita bisa mengirimkan file ejs yang kita request (yang saat ini adalah index.ejs),
+    // ke dalam file main layouts yang kita siapakan (yang saat ini adalah file yang berada di layouts/main-layouts.ejs)
+    res.render('index', { title: "Home", nama: 'Anbia', mahasiswa: mahasiswa, layout: 'layouts/main-layouts' });
 });
 
 // Menjalankan request GET ke halaman about, akan dijalankan fungsi yang memiliki parameter request dan response
 app.get('/about', (req, res) => {
-    res.render('about', { title: "About" });
+    res.render('about', { title: "About", layout: 'layouts/main-layouts' });
 })
 // method sendFile memiliki parameter kedua sebagai object root directory dari file yang ingin kita akses. sedangkan __dirname merupakan lokasi directory dari file app.js
 app.get('/contact', (req, res) => {
-    res.render('contact', { title: "Contact" });
+    res.render('contact', { title: "Contact", layout: 'layouts/main-layouts' });
 })
 
 // Mengecek parameter yang dikirimkan melalui URL
