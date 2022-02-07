d9376fb8c397a7d19b9318ea0449a9e92cab5344
Muhammad Anbia Senggagau <muhammadsenggagau@gmail.com>
2022-02-07 12:36:28 +0700

    _

---
+++ Testing.js
@@ -0,0 +1,25 @@
+const recordCollection={
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+delete recordCollection[2468]['tracks']
+
+let stat=recordCollection[5439].hasOwnProperty('albumTitle')
+
+console.log("".length)
