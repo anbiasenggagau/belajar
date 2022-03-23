32c79edf2235ccb432fe245ac82735789ebeb4f5
Muhammad Anbia Senggagau <muhammadsenggagau@gmail.com>
2022-03-23 08:40:18 +0700

    _

--- Python Project/38. Django Project/product/admin.py
+++ Python Project/38. Django Project/product/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from . models import Product
-from . models import Offer
+from .models import Product
+from .models import Offer
 
 
 # Register your models here.
 
-#Mengatur tampilan tabel pada admin section
+# Mengatur tampilan tabel pada admin section
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','price','stock')
+    list_display = ('name', 'price', 'stock')
+
 
 class OfferAdmin(admin.ModelAdmin):
-    list_display = ('code','discount')
+    list_display = ('code', 'discount')
 
-#Memasukkan database product admin section
+
+# Memasukkan database product admin section
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Offer, OfferAdmin)
